2773e854eccfa844d20a57aa8e689e9c7086a09e
George Higbie <gehigbie@gmail.com>
2017-10-11 21:10:51 -0600

    Add drawable resources

---
+++ app/src/main/java/com/example/georgehigbie/devswag/Services/DataService.kt
@@ -0,0 +1,13 @@
+package com.example.georgehigbie.devswag.Services
+
+import com.example.georgehigbie.devswag.Model.Category
+
+/**
+ * Created by georgehigbie on 10/11/17.
+ */
+
+object DataService {
+    val categories = listOf(
+            Category()
+    )
+}
