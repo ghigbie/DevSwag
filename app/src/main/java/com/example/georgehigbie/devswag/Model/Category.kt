12832f204386bfa3a42c3121a3b751ebf7ab55fe
George Higbie <gehigbie@gmail.com>
2017-10-11 23:02:29 -0600

    Add adpater for data

--- app/src/main/java/com/example/georgehigbie/devswag/Model/Category.kt
+++ app/src/main/java/com/example/georgehigbie/devswag/Model/Category.kt
@@ -4,4 +4,8 @@ package com.example.georgehigbie.devswag.Model
  * Created by georgehigbie on 10/11/17.
  */
 
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String){
+    override fun toString(): String {
+        return title
+    }
+}
