357ea5d187a7cace44ff57a57da2d530fe3d7b25
George Higbie <gehigbie@gmail.com>
2017-10-13 21:17:59 -0600

    Adjusted list view image constraints

---
+++ app/src/main/java/com/example/georgehigbie/devswag/Adapters/CategoryAdapter.kt
@@ -0,0 +1,10 @@
+package com.example.georgehigbie.devswag.Adapters
+
+import android.widget.BaseAdapter
+
+/**
+ * Created by georgehigbie on 10/13/17.
+ */
+class CategoryAdapter : BaseAdapter(){
+
+}
