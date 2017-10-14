e7b81b45077b9a75df077a138f7ce55f2d68f216
George Higbie <gehigbie@gmail.com>
2017-10-13 21:39:58 -0600

    Add custom category adapter

--- app/src/main/java/com/example/georgehigbie/devswag/Adapters/CategoryAdapter.kt
+++ app/src/main/java/com/example/georgehigbie/devswag/Adapters/CategoryAdapter.kt
@@ -1,10 +1,50 @@
 package com.example.georgehigbie.devswag.Adapters
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.georgehigbie.devswag.Model.Category
+import com.example.georgehigbie.devswag.R
 
 /**
  * Created by georgehigbie on 10/13/17.
  */
-class CategoryAdapter : BaseAdapter(){
+class CategoryAdapter(context: Context, categories: List<Category> ) : BaseAdapter(){
+
+    val context = context
+    val categories = categories
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val categoryView : View
+
+        categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+        val categoryImage : ImageView = categoryView.findViewById(R.id.categoryImage)
+        val categoryName : TextView = categoryView.findViewById(R.id.categoryText)
+
+        val category = categories[position]
+
+        val resourceID = context.resources.getIdentifier(category.image, "drawable", context.packageName)
+        categoryImage.setImageResource(resourceID)
+        println(resourceID)
+
+        categoryName.text = category.title
+        return categoryView
+    }
+
+    override fun getItem(position: Int): Any {
+        return categories[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getCount(): Int {
+        return categories.count()
+    }
 
 }
