b32aac36c80c1fb892d691bab90081b0b34ab86b
George Higbie <gehigbie@gmail.com>
2017-10-17 20:52:16 -0600

    Set list adapter to list view

--- app/src/main/java/com/example/georgehigbie/devswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/georgehigbie/devswag/Controller/MainActivity.kt
@@ -2,31 +2,26 @@ package com.example.georgehigbie.devswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.example.georgehigbie.devswag.Adapters.CategoryAdapter
+import com.example.georgehigbie.devswag.Model.Category
 import com.example.georgehigbie.devswag.R
 import com.example.georgehigbie.devswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : CategoryAdapter
+    lateinit var adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.categories)
+        adapter = ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                DataService.categories)
         categoriesList.adapter = adapter
-
-        setOnClickListeners()
     }
 
-    fun setOnClickListeners(){
-        categoriesList.setOnItemClickListener { adapterView, view, i, l ->
-            val category = DataService.categories[i]
-            var message = "You clicked on the ${category} category."
-            Toast.makeText(this, message, Toast.LENGTH_SHORT);
-        }
-    }
 }
