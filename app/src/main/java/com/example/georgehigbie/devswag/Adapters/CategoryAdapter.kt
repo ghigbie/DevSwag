032505e45c36a3897fc7fe9628476d91a6804e70
George Higbie <gehigbie@gmail.com>
2017-10-24 21:11:16 -0600

    Begin new custom adapter

--- app/src/main/java/com/example/georgehigbie/devswag/Adapters/CategoryAdapter.kt
+++ app/src/main/java/com/example/georgehigbie/devswag/Adapters/CategoryAdapter.kt
@@ -13,49 +13,69 @@ import com.example.georgehigbie.devswag.R
 /**
  * Created by georgehigbie on 10/13/17.
  */
-class CategoryAdapter(val context: Context, val categories: List<Category>) : BaseAdapter(){
 
+class CategoryAdapter() : BaseAdapter(){
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val categoryView : View
-        val holder : ViewHolder
-
-        if (convertView == null){
-            categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
-            holder = ViewHolder()
-            holder.categoryImage = categoryView.findViewById(R.id.categoryImage)
-            holder.categoryName = categoryView.findViewById(R.id.categoryText)
-            println("I exist for the first time")
-            categoryView.tag = holder
-        }else{
-            holder = convertView.tag as ViewHolder
-            categoryView = convertView
-            println("GO GREEN!")
-        }
-    
-        val category = categories[position]
-
-        val resourceID = context.resources.getIdentifier(category.image, "drawable", context.packageName)
-        holder.categoryImage?.setImageResource(resourceID)
-        holder.categoryName?.text = category.title
-        return categoryView
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun getItem(position: Int): Any {
-        return categories[position]
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun getItemId(position: Int): Long {
-        return 0
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun getCount(): Int {
-        return categories.count()
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-
-    private class ViewHolder {
-        var categoryImage: ImageView? = null
-        var categoryName: TextView? = null
-    }
 
 }
+//class CategoryAdapter(val context: Context, val categories: List<Category>) : BaseAdapter(){
+//
+//    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+//        val categoryView : View
+//        val holder : ViewHolder
+//
+//        if (convertView == null){
+//            categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+//            holder = ViewHolder()
+//            holder.categoryImage = categoryView.findViewById(R.id.categoryImage)
+//            holder.categoryName = categoryView.findViewById(R.id.categoryText)
+//            println("I exist for the first time")
+//            categoryView.tag = holder
+//        }else{
+//            holder = convertView.tag as ViewHolder
+//            categoryView = convertView
+//            println("GO GREEN!")
+//        }
+//
+//        val category = categories[position]
+//
+//        val resourceID = context.resources.getIdentifier(category.image, "drawable", context.packageName)
+//        holder.categoryImage?.setImageResource(resourceID)
+//        holder.categoryName?.text = category.title
+//        return categoryView
+//    }
+//
+//    override fun getItem(position: Int): Any {
+//        return categories[position]
+//    }
+//
+//    override fun getItemId(position: Int): Long {
+//        return 0
+//    }
+//
+//    override fun getCount(): Int {
+//        return categories.count()
+//    }
+//
+//
+//    private class ViewHolder {
+//        var categoryImage: ImageView? = null
+//        var categoryName: TextView? = null
+//    }
+//
+//}
