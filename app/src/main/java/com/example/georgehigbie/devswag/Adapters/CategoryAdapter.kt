aa2cae5887a1a1450593f70c5ebd96d24b375512
George Higbie <gehigbie@gmail.com>
2017-10-24 21:28:36 -0600

    Rebuild category adapter

--- app/src/main/java/com/example/georgehigbie/devswag/Adapters/CategoryAdapter.kt
+++ app/src/main/java/com/example/georgehigbie/devswag/Adapters/CategoryAdapter.kt
@@ -14,68 +14,34 @@ import com.example.georgehigbie.devswag.R
  * Created by georgehigbie on 10/13/17.
  */
 
-class CategoryAdapter() : BaseAdapter(){
+class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapter() {
+    val context = context
+    val categories = categories
+
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val categoryView: View
+        categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+        val categoryImage: ImageView = categoryView.findViewById(R.id.categoryImage)
+        val categoryName: TextView = categoryView.findViewById(R.id.categoryText)
+
+        val category = categories[position]
+
+        val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName)
+        categoryImage.setImageResource(resourceId)
+        categoryName.text = category.title
+
+        return categoryView
     }
 
     override fun getItem(position: Int): Any {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return categories[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return 0
     }
 
     override fun getCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return categories.count()
     }
-
-
 }
-//class CategoryAdapter(val context: Context, val categories: List<Category>) : BaseAdapter(){
-//
-//    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-//        val categoryView : View
-//        val holder : ViewHolder
-//
-//        if (convertView == null){
-//            categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
-//            holder = ViewHolder()
-//            holder.categoryImage = categoryView.findViewById(R.id.categoryImage)
-//            holder.categoryName = categoryView.findViewById(R.id.categoryText)
-//            println("I exist for the first time")
-//            categoryView.tag = holder
-//        }else{
-//            holder = convertView.tag as ViewHolder
-//            categoryView = convertView
-//            println("GO GREEN!")
-//        }
-//
-//        val category = categories[position]
-//
-//        val resourceID = context.resources.getIdentifier(category.image, "drawable", context.packageName)
-//        holder.categoryImage?.setImageResource(resourceID)
-//        holder.categoryName?.text = category.title
-//        return categoryView
-//    }
-//
-//    override fun getItem(position: Int): Any {
-//        return categories[position]
-//    }
-//
-//    override fun getItemId(position: Int): Long {
-//        return 0
-//    }
-//
-//    override fun getCount(): Int {
-//        return categories.count()
-//    }
-//
-//
-//    private class ViewHolder {
-//        var categoryImage: ImageView? = null
-//        var categoryName: TextView? = null
-//    }
-//
-//}
--- app/src/main/java/com/example/georgehigbie/devswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/georgehigbie/devswag/Controller/MainActivity.kt
@@ -12,15 +12,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-                android.R.layout.simple_list_item_1,
-                DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
 
         categoriesList.adapter = adapter
     }
