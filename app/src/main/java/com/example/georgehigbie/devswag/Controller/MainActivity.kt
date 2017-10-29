02db820950dcce3465f3f78cd1ba1ed94b9d3b58
George Higbie <gehigbie@gmail.com>
2017-10-28 19:51:05 -0600

    Modify gradle

--- app/src/main/java/com/example/georgehigbie/devswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/georgehigbie/devswag/Controller/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.georgehigbie.devswag.Controller
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
 import com.example.georgehigbie.devswag.Adapters.CategoryAdapter
 import com.example.georgehigbie.devswag.R
 import com.example.georgehigbie.devswag.Services.DataService
@@ -19,11 +18,11 @@ class MainActivity : AppCompatActivity() {
         adapter = CategoryAdapter(this, DataService.categories)
 
         categoriesList.adapter = adapter
-        categoriesList.setOnItemClickListener { parent, view, position, id ->
-            val category = DataService.categories[position]
-            var toastMessage = "You clicked on the ${category.title} cell"
-            Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
-        }
+//        categoriesList.setOnItemClickListener { parent, view, position, id ->
+//            val category = DataService.categories[position]
+//            var toastMessage = "You clicked on the ${category.title} cell"
+//            Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
+//        }
     }
 
 }
