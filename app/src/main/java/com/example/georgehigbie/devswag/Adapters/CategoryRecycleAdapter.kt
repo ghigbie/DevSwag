9341626f1bb95438a49041bb59c448f8fda42def
George Higbie <gehigbie@gmail.com>
2017-10-29 19:45:59 -0600

    Add binding function to holder class

--- app/src/main/java/com/example/georgehigbie/devswag/Adapters/CategoryRecycleAdapter.kt
+++ app/src/main/java/com/example/georgehigbie/devswag/Adapters/CategoryRecycleAdapter.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.georgehigbie.devswag.Model.Category
+import com.example.georgehigbie.devswag.R
 
 /**
  * Created by georgehigbie on 10/29/17.
@@ -24,6 +27,15 @@ class CategoryRecycleAdapter(val context: Context, val categories: List<Category
     }
 
     inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+        val categoryImage = itemView?.findViewById<ImageView>(R.id.categoryImage)
+        val categoryName = itemView?.findViewById<TextView>(R.id.categoryText)
 
+        fun bindCategory(category: Category, context: Context){
+            val resourceId = context.resources.getIdentifier(category.image,
+                    "drawable", context.packageName)
+            categoryImage?.setImageResource(resourceId)
+            categoryName?.text = category.title
+
+        }
     }
 }
