8da1da4e1c0e17bdce0854536312aee8f8ea0d66
George Higbie <gehigbie@gmail.com>
2017-10-29 21:07:49 -0600

    Modify data

--- app/src/main/java/com/example/georgehigbie/devswag/Services/DataService.kt
+++ app/src/main/java/com/example/georgehigbie/devswag/Services/DataService.kt
@@ -32,25 +32,25 @@ object DataService {
     )
 
     val hats = listOf(
-            Product("Devsolpes Graphic Beanie", "$18", "hat01"),
-            Product("Devsolpes Hat Blue", "$28", "hat02"),
-            Product("Devsolpes Hat White", "$18", "hat03"),
-            Product("Devsolpes Hat Snapback", "$23", "hat04")
+            Product("Devsolpes Graphic Beanie", "$18", "hat1"),
+            Product("Devsolpes Hat Blue", "$28", "hat2"),
+            Product("Devsolpes Hat White", "$18", "hat3"),
+            Product("Devsolpes Hat Snapback", "$23", "hat4")
     )
 
     val hoodies = listOf(
-            Product("Devslopes Hoodie Gray", "$28", "hoodie01"),
-            Product("Devslopes Hoodie Blue", "$29", "hoodie02"),
-            Product("Devslopes Hoodie Orange", "$32", "hoodie03"),
-            Product("Devslopes Hoodie Black", "$26", "hoodie04")
+            Product("Devslopes Hoodie Gray", "$28", "hoodie1"),
+            Product("Devslopes Hoodie Blue", "$29", "hoodie2"),
+            Product("Devslopes Hoodie Orange", "$32", "hoodie3"),
+            Product("Devslopes Hoodie Black", "$26", "hoodie4")
     )
 
     val shirts = listOf(
-            Product("Devslopes Shirt Gray", "$15", "shirt01"),
-            Product("Devslopes Badge Light Blue", "$16", "shirt02"),
-            Product("Devslopes Logo Shirt Orange", "$16", "shirt03"),
-            Product("Devslopes Hustle", "$18", "shirt04"),
-            Product("Kick Flip Studios", "$19", "shirt05")
+            Product("Devslopes Shirt Gray", "$15", "shirt1"),
+            Product("Devslopes Badge Light Blue", "$16", "shirt2"),
+            Product("Devslopes Logo Shirt Orange", "$16", "shirt3"),
+            Product("Devslopes Hustle", "$18", "shirt4"),
+            Product("Kick Flip Studios", "$19", "shirt5")
     )
 
 
