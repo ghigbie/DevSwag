c72cdee9d781a2ef1dacbec7f1f0abbeb1967da7
George Higbie <gehigbie@gmail.com>
2017-10-29 19:54:42 -0600

    Connect adapter to categories

--- app/src/main/java/com/example/georgehigbie/devswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/georgehigbie/devswag/Controller/MainActivity.kt
@@ -2,27 +2,26 @@ package com.example.georgehigbie.devswag.Controller
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.example.georgehigbie.devswag.Adapters.CategoryAdapter
+import android.support.v7.widget.LinearLayoutManager
+import com.example.georgehigbie.devswag.Adapters.CategoryRecycleAdapter
 import com.example.georgehigbie.devswag.R
 import com.example.georgehigbie.devswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : CategoryAdapter
+    lateinit var adapter : CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.categories)
-
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
         categoriesList.adapter = adapter
-//        categoriesList.setOnItemClickListener { parent, view, position, id ->
-//            val category = DataService.categories[position]
-//            var toastMessage = "You clicked on the ${category.title} cell"
-//            Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
-//        }
+
+        val layoutManager = LinearLayoutManager(this)
+        categoriesList.layoutManager = layoutManager
+        categoriesList.setHasFixedSize(true)
     }
 
 }
