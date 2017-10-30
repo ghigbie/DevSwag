2602b8102a1db026f272a478b8c835f108c7d1fb
George Higbie <gehigbie@gmail.com>
2017-10-29 20:31:49 -0600

    Add products activity

---
+++ app/src/main/java/com/example/georgehigbie/devswag/Controller/ProductsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.georgehigbie.devswag.Controller
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.georgehigbie.devswag.R
+
+class ProductsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_products)
+    }
+}
