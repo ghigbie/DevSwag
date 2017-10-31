13586069617606350b56b6faa9ea2cb070cc02c7
George Higbie <gehigbie@gmail.com>
2017-10-30 21:42:11 -0600

    Add products adapter

---
+++ app/src/main/java/com/example/georgehigbie/devswag/Adapters/ProductsAdapter.kt
@@ -0,0 +1,10 @@
+package com.example.georgehigbie.devswag.Adapters
+
+import android.support.v7.widget.RecyclerView
+
+/**
+ * Created by georgehigbie on 10/30/17.
+ */
+class ProductsAdapter : RecyclerView.Adapter {
+
+}
