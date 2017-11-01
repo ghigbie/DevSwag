a02663594f7ae6fce29c17b7f14fd356b2c100bc
George Higbie <gehigbie@gmail.com>
2017-10-31 22:16:19 -0600

    Add layout manager and set adapter for grid layout

--- app/src/main/java/com/example/georgehigbie/devswag/Controller/ProductsActivity.kt
+++ app/src/main/java/com/example/georgehigbie/devswag/Controller/ProductsActivity.kt
@@ -2,16 +2,26 @@ package com.example.georgehigbie.devswag.Controller
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
+import com.example.georgehigbie.devswag.Adapters.ProductsAdapter
 import com.example.georgehigbie.devswag.R
+import com.example.georgehigbie.devswag.Services.DataService
 import com.example.georgehigbie.devswag.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter : ProductsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)
+        adapter = ProductsAdapter(this, DataService.getProducts(categoryType))
+
+        val layoutManager = GridLayoutManager(this, 2)
+        productsList.layoutManager = layoutManager
+        productsList.adapter = adapter
     }
 }
