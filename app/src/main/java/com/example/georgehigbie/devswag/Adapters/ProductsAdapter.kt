d8422c4faac8b4310c943d876cdef7378be9980c
George Higbie <gehigbie@gmail.com>
2017-10-31 22:09:20 -0600

    Finish products adapater

--- app/src/main/java/com/example/georgehigbie/devswag/Adapters/ProductsAdapter.kt
+++ app/src/main/java/com/example/georgehigbie/devswag/Adapters/ProductsAdapter.kt
@@ -2,27 +2,43 @@ package com.example.georgehigbie.devswag.Adapters
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.georgehigbie.devswag.Model.Product
+import com.example.georgehigbie.devswag.R
 
 /**
  * Created by georgehigbie on 10/30/17.
  */
 class ProductsAdapter(val context : Context, val products: List<Product>) : RecyclerView.Adapter<ProductsAdapter.ProductHolder>() {
     override fun onBindViewHolder(holder: ProductHolder?, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder?.bindProduct(products[position], context)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ProductHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view = LayoutInflater.from(context).inflate(R.layout.product_list_item, parent, false)
+        return ProductHolder(view)
     }
 
     override fun getItemCount(): Int {
         return products.count()
     }
 
-    inner class ProductHolder(itemView: View?) : RecyclerView.ViewHolder(itemView)
+    inner class ProductHolder(itemView: View?) : RecyclerView.ViewHolder(itemView){
+        val productImage = itemView?.findViewById<ImageView>(R.id.productImage)
+        val productName = itemView?.findViewById<TextView>(R.id.productName)
+        val productPrice = itemView?.findViewById<TextView>(R.id.productPrice)
 
+        fun bindProduct(product: Product, context: Context){
+            val resourceId = context.resources.getIdentifier(product.image, "drawable", context.packageName)
+            productImage?.setImageResource(resourceId)
+            productName?.text = product.title
+            productPrice?.text = product.price
+
+        }
     }
+
 }
