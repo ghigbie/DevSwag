ab7387097d1473af5d8571aa749f97efcb475684
George Higbie <gehigbie@gmail.com>
2017-10-31 21:46:29 -0600

    Add members to ProductsAdapter

--- app/src/main/java/com/example/georgehigbie/devswag/Adapters/ProductsAdapter.kt
+++ app/src/main/java/com/example/georgehigbie/devswag/Adapters/ProductsAdapter.kt
@@ -1,6 +1,28 @@
 package com.example.georgehigbie.devswag.Adapters
 
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import com.example.georgehigbie.devswag.Model.Product
+
 /**
  * Created by georgehigbie on 10/30/17.
  */
-class ProductsAdapter{}
+class ProductsAdapter(val context : Context, val products: List<Product>) : RecyclerView.Adapter<ProductsAdapter.ProductHolder>() {
+    override fun onBindViewHolder(holder: ProductHolder?, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ProductHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        return products.count()
+    }
+
+    inner class ProductHolder(itemView: View?) : RecyclerView.ViewHolder(itemView)
+
+    }
+}
