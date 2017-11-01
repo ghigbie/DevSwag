4475cb32d068227da6159485b35ab4cf35df1ee8
George Higbie <gehigbie@gmail.com>
2017-10-31 22:22:11 -0600

    Add duplicate data to help with recycler view

--- app/src/main/java/com/example/georgehigbie/devswag/Services/DataService.kt
+++ app/src/main/java/com/example/georgehigbie/devswag/Services/DataService.kt
@@ -35,6 +35,14 @@ object DataService {
             Product("Devsolpes Graphic Beanie", "$18", "hat1"),
             Product("Devsolpes Hat Blue", "$28", "hat2"),
             Product("Devsolpes Hat White", "$18", "hat3"),
+            Product("Devsolpes Hat Snapback", "$23", "hat4"),
+            Product("Devsolpes Graphic Beanie", "$18", "hat1"),
+            Product("Devsolpes Hat Blue", "$28", "hat2"),
+            Product("Devsolpes Hat White", "$18", "hat3"),
+            Product("Devsolpes Hat Snapback", "$23", "hat4"),
+            Product("Devsolpes Graphic Beanie", "$18", "hat1"),
+            Product("Devsolpes Hat Blue", "$28", "hat2"),
+            Product("Devsolpes Hat White", "$18", "hat3"),
             Product("Devsolpes Hat Snapback", "$23", "hat4")
     )
 
@@ -42,10 +50,28 @@ object DataService {
             Product("Devslopes Hoodie Gray", "$28", "hoodie1"),
             Product("Devslopes Hoodie Blue", "$29", "hoodie2"),
             Product("Devslopes Hoodie Orange", "$32", "hoodie3"),
+            Product("Devslopes Hoodie Black", "$26", "hoodie4"),
+            Product("Devslopes Hoodie Gray", "$28", "hoodie1"),
+            Product("Devslopes Hoodie Blue", "$29", "hoodie2"),
+            Product("Devslopes Hoodie Orange", "$32", "hoodie3"),
+            Product("Devslopes Hoodie Black", "$26", "hoodie4"),
+            Product("Devslopes Hoodie Gray", "$28", "hoodie1"),
+            Product("Devslopes Hoodie Blue", "$29", "hoodie2"),
+            Product("Devslopes Hoodie Orange", "$32", "hoodie3"),
             Product("Devslopes Hoodie Black", "$26", "hoodie4")
     )
 
     val shirts = listOf(
+            Product("Devslopes Shirt Gray", "$15", "shirt1"),
+            Product("Devslopes Badge Light Blue", "$16", "shirt2"),
+            Product("Devslopes Logo Shirt Orange", "$16", "shirt3"),
+            Product("Devslopes Hustle", "$18", "shirt4"),
+            Product("Kick Flip Studios", "$19", "shirt5"),
+            Product("Devslopes Shirt Gray", "$15", "shirt1"),
+            Product("Devslopes Badge Light Blue", "$16", "shirt2"),
+            Product("Devslopes Logo Shirt Orange", "$16", "shirt3"),
+            Product("Devslopes Hustle", "$18", "shirt4"),
+            Product("Kick Flip Studios", "$19", "shirt5"),
             Product("Devslopes Shirt Gray", "$15", "shirt1"),
             Product("Devslopes Badge Light Blue", "$16", "shirt2"),
             Product("Devslopes Logo Shirt Orange", "$16", "shirt3"),
